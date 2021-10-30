30a23ee05e7fed86e0141c4cb9ebae85129c96d5
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-10-30 18:44:54 -0400

    README and uvindex colors

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # weatherdashboard-serversideAPIs
 View of current and future weather via API call to Open Weather
+
+![workday schedule page] REMEMBER TO ADD PHOTO
+## Description
+This application is designed for travelers who want ot see the weather outlook for mutliple cities which will help them to better plan their trips.  
+
+**features**
+ * Abililty to enter city name to retreive the forecast
+ * Current and future- 5 day forecasts are displayed
+ * Critical details including temperature and a weather icon- for a quick glance
+ * Also, key packing information like UV Index(sunscreen and sunglasses), wind speed (an extra jacket) and lastly humidity (bad hair- oh no!)
+ * Weather data is obtained via API from Open Weather for both the weather information and the geo translation for latitude and longitude
+ * Moment.js for date manipulations
+ * Bootstrap is used for formatting of the applicationb
+ * Complete READme file
+ 
+
+**future features**
+ * Suggestions on best places to visit
+ * Packing suggestions
+ * Direct access to a map application to find the nearest place to visit
+
+
+
+## Executing Program
+If you would like to view , you can access full code and documentation by visiting Dawn LeBlanc's Git Hub Repo  https://github.com/dawnleblanc11/weatherdashboard-serversideAPIsor view directly via  https://dawnleblanc11.github.io/weatherdashboard-serversideAPIs/
+
+## Help
+All verification of usage was completed on a Google Chrome browser.
+
+## Authors
+Inital Code from Bootcamp
+Revisions Dawn LeBlanc 10-2021
+
+## Version History
+1.0 Intital Commit
+1.1 API Call set up
+1.2 Switch to Farenheit
+1.3 geo fetch for latitude and longitude
+1.4 added moment for dates
+1.5 all data captured
+
+
+## License
+N/A
+
+## Acknowledgements
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // TO Do: Work on Format for page
-// Add color ranges for UV Index
-// Assure icons tranlate in format
+// make color block smaller for background color//test other colors work
 // Local storage of city entered
+// Fix bootstrap formatting
+// make all weather cards dunamic and loop through to appear- shorter code
 // Repopulate screen with local storage items- see work day calendar (sort by alpha or most recent)- determine max length
 // Filter duplicates from storage
 // READme
@@ -9,19 +10,6 @@
 
 function myFunction() {
   var searchCity = document.getElementById("searchcity").value;
-  // Add Date information with moment.js
-  var today = moment();
-  var currentDate = moment(today).format("ll");
-  var currentp1Date = moment(today, "DD-MM-YYY").add(1, "days");
-  var currentp1Datef = moment(currentp1Date).format("ll");
-  var currentp2Date = moment(today, "DD-MM-YYY").add(2, "days");
-  var currentp2Datef = moment(currentp2Date).format("ll");
-  var currentp3Date = moment(today, "DD-MM-YYY").add(3, "days");
-  var currentp3Datef = moment(currentp3Date).format("ll");
-  var currentp4Date = moment(today, "DD-MM-YYY").add(4, "days");
-  var currentp4Datef = moment(currentp4Date).format("ll");
-  var currentp5Date = moment(today, "DD-MM-YYY").add(5, "days");
-  var currentp5Datef = moment(currentp5Date).format("ll");
 
   fetch(
     //API to translate the city entered into latitude and longitude
@@ -38,7 +26,7 @@ function myFunction() {
       var cityLongitude = geoResponse[0].lon;
       console.log(cityLatitude);
       console.log(cityLongitude);
-      //API to gather all the current and 4 day forecast based on latitude and logitude returned
+      //API to gather all the current and 5 day forecast based on latitude and logitude returned
       return fetch(
         "https://api.openweathermap.org/data/2.5/onecall?lat=" +
           cityLatitude +
@@ -52,48 +40,174 @@ function myFunction() {
     })
     .then(function (response) {
       //verified values all correct
-      console.log(currentDate);
-      // include date from currentDate
-      console.log(response.daily[0].temp.day);
+
+      //start Current Weather Section
+      var today = moment();
+      var currentDate = moment(today).format("ll");
+      //date from moment.js
+      var currentweathericon = response.daily[0].weather[0].icon;
+      var currentweathericonURL =
+        "http://api.openweathermap.org/img/w/" + currentweathericon + ".png";
+      // http://api.openweathermap.org/img/w/*.png replace * with code of the icon
+      $("#currentcitydateicon").html(
+        searchCity +
+          "  " +
+          currentDate +
+          "  " +
+          "<img id='wicon' src=" +
+          currentweathericonURL +
+          " alt='WeatherIcon'>"
+      );
+      var currenttemp = response.daily[0].temp.day;
+      $("#temp").text("Temp: " + currenttemp + "°F");
       //add dgree symbol and F to the end- no rounding needed
-      console.log(response.daily[0].wind_speed);
+      var currentwindspeed = response.daily[0].wind_speed;
+      $("#wind").text("Wind: " + currentwindspeed + " MPH");
       //wind speed MPH
-      console.log(response.daily[0].humidity);
+      var currenthumidity = response.daily[0].humidity.toPrecision(2);
+      $("#humidity").text("Humidity: " + currenthumidity + "%");
       //round to no decimals and % symbol to the end
-      console.log(response.daily[0].uvi);
+      var currentuvindex = response.daily[0].uvi;
+      $("#uvindex").text("UV Index: " + currentuvindex);
+
+      if (currentuvindex<3) {
+        $("#uvindex").css("background-color","green");
+      } else if(currentuvindex>=3||currentuvindex<6) {
+        $("#uvindex").css("background-color","yellow");
+      } else if (currentuvindex>=6||currentuvindex<8) {
+      $("#uvindex").css("background-color","orange");
+      } else if (currentuvindex>=8||currentuvindex<11) {
+      $("#uvindex").css("background-color","red");
+      } else {
+      $("#uvindex").css("background-color","purple");
+      }
+
+      //  ToDo: Set Background color
       //  Based on UV Index scale CDC - 0-2 minimal- green, 3-5 low- yellow, 6-7 moderate- orange, 8-10 high- red, >11 extreme- purple
       //  https://www.aimatmelanoma.org/melanoma-101/prevention/what-is-ultraviolet-uv-radiation/- SRC for color variations
-      console.log(currentp1Datef);
-      console.log(response.daily[1].weather[0].icon);
+      //end Current Weather Section
+      //TO DO: Add Section Header Here
+      //start Day Plus 1 Section
+      var currentp1Date = moment(today, "DD-MM-YYY").add(1, "days");
+      var currentp1Datef = moment(currentp1Date).format("ll");
+      //date from moment.js
+      var p1weathericon = response.daily[1].weather[0].icon;
+      var p1weathericonURL =
+        "http://api.openweathermap.org/img/w/" + p1weathericon + ".png";
       // http://api.openweathermap.org/img/w/*.png replace * with code of the icon
-      console.log(response.daily[1].temp.day);
-      console.log(response.daily[1].wind_speed);
-      console.log(response.daily[1].humidity);
-      console.log(currentp2Datef);
-      console.log(response.daily[2].weather[0].icon);
-      console.log(response.daily[2].temp.day); 
-      console.log(response.daily[2].wind_speed);
-      console.log(response.daily[2].humidity);
-      console.log(currentp3Datef);
-      console.log(response.daily[3].weather[0].icon);
-      console.log(response.daily[3].temp.day);
-      console.log(response.daily[3].wind_speed);
-      console.log(response.daily[3].humidity);
-      console.log(currentp4Datef);
-      console.log(response.daily[4].weather[0].icon);
-      console.log(response.daily[4].temp.day);
-      console.log(response.daily[4].wind_speed);
-      console.log(response.daily[4].humidity);
-      console.log(currentp5Datef);
-      console.log(response.daily[4].weather[0].icon);
-      console.log(response.daily[4].temp.day);
-      console.log(response.daily[4].wind_speed);
-      console.log(response.daily[4].humidity);
-
-      // var responseContainerEl = document.querySelector('#response-container');
-      // responseContainerEl.innerHTML = '';
-      // var gifImg = document.createElement('img');
-      // gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
-      // responseContainerEl.appendChild(gifImg);
+      $("#p1date").html(
+        currentp1Datef +
+          "  " +
+          "<img id='wicon' src=" +
+          p1weathericonURL +
+          " alt='WeatherIcon'>"
+      );
+      var p1temp = response.daily[1].temp.day;
+      $("#p1temp").text("Temp: " + p1temp + "°F");
+      //add dgree symbol and F to the end- no rounding needed
+      var p1windspeed = response.daily[1].wind_speed;
+      $("#p1wind").text("Wind: " + p1windspeed + " MPH");
+      //wind speed MPH
+      var p1humidity = response.daily[1].humidity.toPrecision(2);
+      $("#p1humidity").text("Humidity: " + p1humidity + "%");
+      //round to no decimals and % symbol to the end
+      // end Day Plus 1 Section
+      // start Day Plus 2 Section
+      var currentp2Date = moment(today, "DD-MM-YYY").add(2, "days");
+      var currentp2Datef = moment(currentp2Date).format("ll");
+      //date from moment.js
+      var p2weathericon = response.daily[2].weather[0].icon;
+      var p2weathericonURL =
+        "http://api.openweathermap.org/img/w/" + p2weathericon + ".png";
+      // http://api.openweathermap.org/img/w/*.png replace * with code of the icon
+      $("#p2date").html(
+        currentp2Datef +
+          "  " +
+          "<img id='wicon' src=" +
+          p2weathericonURL +
+          " alt='WeatherIcon'>"
+      );
+      var p2temp = response.daily[2].temp.day;
+      $("#p2temp").text("Temp: " + p2temp + "°F");
+      //add dgree symbol and F to the end- no rounding needed
+      var p2windspeed = response.daily[2].wind_speed;
+      $("#p2wind").text("Wind: " + p2windspeed + " MPH");
+      //wind speed MPH
+      var p2humidity = response.daily[2].humidity.toPrecision(2);
+      $("#p2humidity").text("Humidity: " + p2humidity + "%");
+      //round to no decimals and % symbol to the end
+      //end Day Plus 2 Section
+      //start Day Plus 3 Section
+      var currentp3Date = moment(today, "DD-MM-YYY").add(3, "days");
+      var currentp3Datef = moment(currentp3Date).format("ll");
+      var p3weathericon = response.daily[3].weather[0].icon;
+      var p3weathericonURL =
+        "http://api.openweathermap.org/img/w/" + p3weathericon + ".png";
+      // http://api.openweathermap.org/img/w/*.png replace * with code of the icon
+      $("#p3date").html(
+        currentp3Datef +
+          "  " +
+          "<img id='wicon' src=" +
+          p3weathericonURL +
+          " alt='WeatherIcon'>"
+      );
+      var p3temp = response.daily[3].temp.day;
+      $("#p3temp").text("Temp: " + p3temp + "°F");
+      //add dgree symbol and F to the end- no rounding needed
+      var p3windspeed = response.daily[3].wind_speed;
+      $("#p3wind").text("Wind: " + p3windspeed + " MPH");
+      //wind speed MPH
+      var p3humidity = response.daily[3].humidity.toPrecision(2);
+      $("#p3humidity").text("Humidity: " + p3humidity + "%");
+      //round to no decimals and % symbol to the end
+      //end Day Plus 3 Section
+      //start Day Plus 4 Section
+      var currentp4Date = moment(today, "DD-MM-YYY").add(4, "days");
+      var currentp4Datef = moment(currentp4Date).format("ll");
+      var p4weathericon = response.daily[4].weather[0].icon;
+      var p4weathericonURL =
+        "http://api.openweathermap.org/img/w/" + p4weathericon + ".png";
+      // http://api.openweathermap.org/img/w/*.png replace * with code of the icon
+      $("#p4date").html(
+        currentp4Datef +
+          "  " +
+          "<img id='wicon' src=" +
+          p4weathericonURL +
+          " alt='WeatherIcon'>"
+      );
+      var p4temp = response.daily[4].temp.day;
+      $("#p4temp").text("Temp: " + p4temp + "°F");
+      //add dgree symbol and F to the end- no rounding needed
+      var p4windspeed = response.daily[4].wind_speed;
+      $("#p4wind").text("Wind: " + p4windspeed + " MPH");
+      //wind speed MPH
+      var p4humidity = response.daily[4].humidity.toPrecision(2);
+      $("#p4humidity").text("Humidity: " + p4humidity + "%");
+      //round to no decimals and % symbol to the end
+      // end Day Plus 4 section
+      // start Day Plus 5 Section
+      var currentp5Date = moment(today, "DD-MM-YYY").add(5, "days");
+      var currentp5Datef = moment(currentp5Date).format("ll");
+      var p5weathericon = response.daily[5].weather[0].icon;
+      var p5weathericonURL =
+        "http://api.openweathermap.org/img/w/" + p5weathericon + ".png";
+      // http://api.openweathermap.org/img/w/*.png replace * with code of the icon
+      $("#p5date").html(
+        currentp5Datef +
+          "  " +
+          "<img id='wicon' src=" +
+          p5weathericonURL +
+          " alt='WeatherIcon'>"
+      );
+      var p5temp = response.daily[5].temp.day;
+      $("#p5temp").text("Temp: " + p5temp + "°F");
+      //add dgree symbol and F to the end- no rounding needed
+      var p5windspeed = response.daily[5].wind_speed;
+      $("#p5wind").text("Wind: " + p5windspeed + " MPH");
+      //wind speed MPH
+      var p5humidity = response.daily[5].humidity.toPrecision(2);
+      $("#p5humidity").text("Humidity: " + p5humidity + "%");
+      //round to no decimals and % symbol to the end
+      // end Day Plus 5 Section
     });
 }
